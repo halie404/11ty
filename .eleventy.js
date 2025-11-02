159dcbf961467758d05439d326830f07354afbb5
Halima Dinle <26465999+halie404@users.noreply.github.com>
2025-11-02 12:40:44 +0100

    #1 setup

---
+++ .eleventy.js
@@ -0,0 +1,9 @@
+export default {
+  dir: {
+    input: ".", // root folder
+    includes: "_includes", // folder with base.njk
+  },
+  markdownTemplateEngine: "njk",
+  htmlTemplateEngine: "njk",
+  dataTemplateEngine: "njk",
+};
